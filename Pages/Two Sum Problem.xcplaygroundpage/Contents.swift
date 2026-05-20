51e1a68a122dde200fe71ed9ec74a80ed10a68be
Shubham <kapoor.shubham58@outlook.com>
2026-05-20 18:29:24 +0530

    2 Sum Problem

--- Pages/Two Sum Problem.xcplaygroundpage/Contents.swift
+++ Pages/Two Sum Problem.xcplaygroundpage/Contents.swift
@@ -16,7 +16,9 @@ import Foundation
 //
 //  2 + 7 = 9
 
-
+/// Brute Force Techniqueue
+/// Time Complexity O(n2)
+///Space Complexity O(1)
 func twoSum1(_ array: [Int], _ target: Int) -> [Int]? {
     for i in 0..<array.count {
         for j in i+1..<array.count {
@@ -30,3 +32,23 @@ func twoSum1(_ array: [Int], _ target: Int) -> [Int]? {
 
 let result1: [Int]? = twoSum1([2,7,11,15], 9)
 print("Two Sum Result 1: \(result1)")
+
+
+//Solution2: Hash Table
+///Time Complexity average is Average Case → O(1)
+///Worst Case → O(n)
+///
+func twoSum2(_ array: [Int], _ target: Int) -> [Int]? {
+    var hashTable: [Int: Int] = [:]
+    for (index, num) in array.enumerated() {
+        let complement = target - num
+        if let foundIndex = hashTable[complement] {
+            return [foundIndex, index]
+        }
+        hashTable[num] = index
+    }
+    return nil
+}
+
+let result2: [Int]? = twoSum2([2,7,11,15], 9)
+print("Two Sum Result 2: \(result2)")
